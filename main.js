76bb5e0696d9ca9b06bdf6bb7fc35d6d84ca39d4
SucioLoco <irelandrj@yahoo.com>
2021-10-13 18:08:21 -0400

    first commit

---
+++ main.js
@@ -0,0 +1,8 @@
+const root = document.getElementById("root")
+
+root.innerHTML = `
+<h1>Currency Converter</h1>
+<p>Convert USD to
+<select name="currency-converters" id="currency-converters" </select>
+</p>
+`
