b83a6164024612adb1e146c9bc4be89290c597c5
SucioLoco <irelandrj@yahoo.com>
2021-10-14 09:06:12 -0400

    made a select button

--- script.js
+++ script.js
@@ -35,18 +35,19 @@ const currencies = [
 ]
 
 const root = document.getElementById("root")
+const container = document.getElementById("container")
 
-
+root.appendChild(container)
 root.innerHTML = `<h1>Currency Converter</h1>
 <p>Convert from USD to</p>
 <div class="dropdown-menu" id="dropdown-menu>
-<label for="currencyMenu" id="currencyMenu"></label>
-<input id="currencyMenu" type=text></input>
+<label for="currencyMenu" id="currencyMenu">Choose Currency</label>
+<select id="currencyMenu">Currency</select>
 <button id="submit">submit</button>
 `
 
 
-let selectConverter = document.getElementById("currency-converter")
+// let selectConverter = document.getElementById("currency-converter")
 
 
 for (let currency of currencies) {
